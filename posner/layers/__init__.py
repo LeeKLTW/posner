f6633c57701ec509b263956953871492dcc111ea
LeeKLTW <LeeKLTW@gmail.com>
2020-02-06 18:23:47 +0800

    register layers

---
+++ posner/layers/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from .layer_normalization import LayerNormalization
+from .scaled_dot_product_attention import ScaledDotProductAttention
+from .multi_head_attention import MultiHeadAttention
+from .position_embedding import PositionEmbedding
