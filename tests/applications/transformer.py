8b9c9f16aeaa4f0c27382ca5b7c49a196fb43155
LeeKLTW <LeeKLTW@gmail.com>
2020-02-07 17:04:28 +0800

    test get encoder

---
+++ tests/applications/transformer.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import unittest
+from tensorflow import keras
+from posner.applications import get_encoders
+
+
+class TestGetEncoderComponent(unittest.TestCase):
+
+  def test_sample(self):
+    input_layer = keras.layers.Input(shape=(512, 768), name='Input')
+    output_layer = get_encoders(
+      encoder_num=2,
+      input_layer=input_layer,
+      head_num=12,
+      hidden_dim=3072,
+      dropout_rate=0.1,
+    )
+    model = keras.models.Model(inputs=input_layer, outputs=output_layer)
+    model.compile(optimizer='adam', loss='mse', metrics={})
+    model.summary(line_length=160)
+    self.assertIsNotNone(model)
