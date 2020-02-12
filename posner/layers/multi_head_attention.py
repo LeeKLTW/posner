78d3b27bcdfb6dcee517e20c8be83c8f00a60771
LeeKLTW <LeeKLTW@gmail.com>
2020-02-12 20:29:00 +0800

    add Class RelativeAttention, Class RelativeMultiHeadAttention

--- posner/layers/multi_head_attention.py
+++ posner/layers/multi_head_attention.py
@@ -6,7 +6,7 @@
 .. moduleauthor:: LeeKLTW
 
 """
-
+import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import backend as K
 from . import ScaledDotProductAttention
@@ -241,3 +241,169 @@ class MultiHeadAttention(keras.layers.Layer):
     mask = K.tile(mask, [1, head_num, 1])
     return K.reshape(mask, (-1,seq_len))
 
+#TODO: integrate to attention.py
+class RelativeAttention(tf.keras.layers.Layer):
+  """Core calculations for relative attention."""
+
+  def __init__(self, dropout_att, scale):
+    super(RelativeAttention, self).__init__()
+    self.scale = scale
+    self.dropout_att = dropout_att
+
+  def build(self, unused_input_shapes):
+    """Implements build() for the layer."""
+
+    self.attention_probs_dropout = tf.keras.layers.Dropout(
+        rate=self.dropout_att)
+
+    super(RelativeAttention, self).build(unused_input_shapes)
+
+  def call(self, q_head, k_head_h, v_head_h, k_head_r, seg_embed, seg_mat,
+           r_w_bias, r_r_bias, r_s_bias, attn_mask):
+    """Implements call() for the layer."""
+
+    # content based attention score
+    ac = tf.einsum('ibnd,jbnd->ijbn', q_head + r_w_bias, k_head_h)
+
+    # position based attention score
+    bd = tf.einsum('ibnd,jbnd->ijbn', q_head + r_r_bias, k_head_r)
+    bd = RelativeAttention.rel_shift(bd, klen=tf.shape(ac)[1])
+
+    # segment-based attention score
+    if seg_mat is None:
+      ef = 0
+    else:
+      ef = tf.einsum('ibnd,snd->isbn', q_head + r_s_bias, seg_embed)
+      tgt_shape = tf.shape(bd)
+      ef = tf.where(
+          tf.broadcast_to(tf.expand_dims(seg_mat, 3), tgt_shape),
+          tf.broadcast_to(ef[:, 1:, :, :], tgt_shape),
+          tf.broadcast_to(ef[:, :1, :, :], tgt_shape))
+
+    # merges attention scores and performs masking
+    attn_score = (ac + bd + ef) * self.scale
+    if attn_mask is not None:
+      attn_score = attn_score - 1e30 * attn_mask
+
+    # attention probability
+    attn_prob = tf.nn.softmax(attn_score, 1)
+    attn_prob = self.attention_probs_dropout(attn_prob)
+
+    # attention output
+    attn_vec = tf.einsum('ijbn,jbnd->ibnd', attn_prob, v_head_h)
+
+    return attn_vec
+
+  @staticmethod
+  def rel_shift(x, klen=-1):
+    """Performs relative shift to form the relative attention score."""
+    x_size = tf.shape(x)
+
+    x = tf.reshape(x, [x_size[1], x_size[0], x_size[2], x_size[3]])
+    x = tf.slice(x, [1, 0, 0, 0], [-1, -1, -1, -1])
+    x = tf.reshape(x, [x_size[0], x_size[1] - 1, x_size[2], x_size[3]])
+    x = tf.slice(x, [0, 0, 0, 0], [-1, klen, -1, -1])
+
+    return x
+
+class RelativeMultiheadAttention(tf.keras.layers.Layer):
+  """Multi-head attention with relative embedding."""
+
+  def __init__(self, d_model, n_head, d_head, dropout, dropout_att,
+               kernel_initializer, **kwargs):
+    super(RelativeMultiheadAttention, self).__init__(**kwargs)
+    self.d_model = d_model
+    self.n_head = n_head
+    self.d_head = d_head
+    self.dropout = dropout
+    self.dropout_att = dropout_att
+    self.initializer = kernel_initializer
+
+  def build(self, unused_input_shapes):
+    """Implements build() for the layer."""
+    self.scale = 1.0 / (self.d_head ** 0.5)
+
+    self.output_layer_norm = tf.keras.layers.LayerNormalization(
+      name='LayerNorm', axis=-1, epsilon=1e-12)
+
+    self.kh_projection_layer = self.add_weight(
+      'k/kernel',
+      shape=[self.d_model, self.n_head, self.d_head],
+      initializer=self.initializer)
+    self.vh_projection_layer = self.add_weight(
+      'v/kernel',
+      shape=[self.d_model, self.n_head, self.d_head],
+      initializer=self.initializer)
+    self.kr_projection_layer = self.add_weight(
+      'r/kernel',
+      shape=[self.d_model, self.n_head, self.d_head],
+      initializer=self.initializer)
+    self.qh_projection_layer = self.add_weight(
+      'q/kernel',
+      shape=[self.d_model, self.n_head, self.d_head],
+      initializer=self.initializer)
+
+    self.relative_attention_layer = RelativeAttention(
+      dropout_att=self.dropout_att, scale=self.scale)
+
+    self.proj_o = self.add_weight(
+      'o/kernel',
+      shape=[self.d_model, self.n_head, self.d_head],
+      initializer=self.initializer)
+
+    self.attention_dropout = tf.keras.layers.Dropout(rate=self.dropout)
+
+    super(RelativeMultiheadAttention, self).build(unused_input_shapes)
+
+  def call(self, h, g, r, r_w_bias, r_r_bias, seg_mat, r_s_bias, seg_embed,
+           attn_mask_h, attn_mask_g, mems, target_mapping):
+    """Implements call() for the layer."""
+
+    if mems is not None and mems.shape.ndims > 1:
+      cat = tf.concat([mems, h], 0)
+    else:
+      cat = h
+
+    # content heads
+    q_head_h = tf.einsum('ibh,hnd->ibnd', h, self.qh_projection_layer)
+    k_head_h = tf.einsum('ibh,hnd->ibnd', cat, self.kh_projection_layer)
+    v_head_h = tf.einsum('ibh,hnd->ibnd', cat, self.vh_projection_layer)
+
+    # positional heads
+    k_head_r = tf.einsum('ibh,hnd->ibnd', r, self.kr_projection_layer)
+
+    # core attention ops
+    attn_vec_h = self.relative_attention_layer(q_head_h, k_head_h, v_head_h,
+                                               k_head_r, seg_embed, seg_mat,
+                                               r_w_bias, r_r_bias, r_s_bias,
+                                               attn_mask_h)
+
+    # post processing
+    output_h = tf.einsum('ibnd,hnd->ibh', attn_vec_h, self.proj_o)
+    output_h = self.attention_dropout(output_h)
+    output_h = self.output_layer_norm(output_h + h)
+
+    output_g = None
+    if g is not None:  # enable two-stream attention
+      # g-stream
+      q_head_g = tf.einsum('ibh,hnd->ibnd', g, self.qh_projection_layer)
+      if target_mapping is not None:
+        q_head_g = tf.einsum('mbnd,mlb->lbnd', q_head_g, target_mapping)
+        attn_vec_g = self.relative_attention_layer(q_head_g, k_head_h, v_head_h,
+                                                   k_head_r, seg_embed, seg_mat,
+                                                   r_w_bias, r_r_bias, r_s_bias,
+                                                   attn_mask_g)
+        attn_vec_g = tf.einsum('lbnd,mlb->mbnd', attn_vec_g, target_mapping)
+
+      else:
+        attn_vec_g = self.relative_attention_layer(q_head_g, k_head_h, v_head_h,
+                                                   k_head_r, seg_embed, seg_mat,
+                                                   r_w_bias, r_r_bias, r_s_bias,
+                                                   attn_mask_g)
+
+      # post processing
+      output_g = tf.einsum('ibnd,hnd->ibh', attn_vec_g, self.proj_o)
+      output_g = self.attention_dropout(output_g)
+      output_g = self.output_layer_norm(output_g + g)
+
+    return (output_h, output_g)
