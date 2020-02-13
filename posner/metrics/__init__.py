8b069b404c1dbbdbc9881f2ab9279f78e7d34c4c
LeeKLTW <LeeKLTW@gmail.com>
2020-02-13 19:12:43 +0800

    add metrics recall, precision, f1

--- posner/metrics/__init__.py
+++ posner/metrics/__init__.py
@@ -1,2 +1,3 @@
 # -*- coding: utf-8 -*-
 from .crf_accuracy import crf_accuracy, crf_marginal_accuracy, crf_viterbi_accuracy
+from .f1 import precision,recall,f1
---
+++ posner/metrics/f1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from keras import backend as K
+
+def recall(y_true, y_pred):
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+        recall = true_positives / (possible_positives + K.epsilon())
+        return recall
+
+def precision(y_true, y_pred):
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+        precision = true_positives / (predicted_positives + K.epsilon())
+        return precision
+
+def f1(y_true, y_pred):
+    precision_ = precision(y_true, y_pred)
+    recall_ = recall(y_true, y_pred)
+    return 2*((precision_*recall_)/(precision_+recall_+K.epsilon()))
